8c381690b82651e6fbdd750d86958951ab71b444
erdivartanovich <erdivartanovich@gmail.com>
2023-05-02 14:10:04 +0700

    add kitten script wrapper

---
+++ session_switcher.py
@@ -0,0 +1,14 @@
+from typing import List
+from kitty.boss import Boss
+from kittens.tui.handler import result_handler
+
+
+def main(args: List[str]) -> str:
+    pass
+
+
+@result_handler(no_ui=True)
+def handle_result(args: List[str], answer: str, target_window_id: int, boss: Boss) -> None:
+    w = boss.active_window
+    if w is not None:
+        boss.call_remote_control(w, ('launch', 'session_switcher.sh'))
