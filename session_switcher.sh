97bc9c4569efee4d81064a1d05c4a002c3ab0776
erdivartanovich <erdivartanovich@gmail.com>
2023-05-02 12:23:13 +0700

    add sh script to switch between kitty session

---
+++ session_switcher.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Kitty Session Switcher
+## written by @erdivartanovich
+## email: erdi@monommania.com
+
+app_name="session_switcher.sh"
+pcount=$(pgrep -fc "$app_name")
+if [ "$pcount" -gt 2 ]; then
+  exit 0
+fi
+
+fzf_command=(fzf --margin=30% --reverse --header-first --header="Select Session" --border "--query=$1" "-1")
+session=$(
+  kitty @ ls | jq ".[] |  .tabs[0].windows[] | select(.title != \"$app_name\")" | jq -r '"\(.id):\t\(.title)\t\(.cwd)"' |
+    "${fzf_command[@]}"
+)
+if [ -z "$session" ]; then
+  echo >&2 "No session selected"
+else
+  selected=$(echo "$session" | cut -d':' -f1)
+  id="${selected//[^0-9]/}"
+  kitty @ focus-window -m id:"$id"
+fi
